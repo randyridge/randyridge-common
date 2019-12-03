b357c7a94b02feac528bfc4be552d03554b3524d
Randy Ridge <randyridge@gmail.com>
2019-12-03 16:43:11 -0500

    finishing project setup

---
+++ RandyRidge.Common.Benchmarks/GuardBenchmarks.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace RandyRidge.Common {
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [RPlotExporter, RankColumn]
+    public class GuardBenchmarks {
+        [Benchmark(Baseline = true)]
+        public void GuardWithout() {
+            Guard.ArgumentNotNull(string.Empty, "argument");
+        }
+    }
+}
---
+++ RandyRidge.Common.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace RandyRidge.Common {
+    internal static class Program {
+        private static void Main() {
+            var summary = BenchmarkRunner.Run<GuardBenchmarks>();
+        }
+    }
+}
