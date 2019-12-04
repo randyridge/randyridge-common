c05a08a41a929c7a9a8e4f7ab69ac7b0f99fc5d0
Randy Ridge <randyridge@gmail.com>
2019-12-03 21:23:29 -0500

    fxcop

--- RandyRidge.Common.Benchmarks/Program.cs
+++ RandyRidge.Common.Benchmarks/Program.cs
@@ -3,7 +3,7 @@
 namespace RandyRidge.Common {
     internal static class Program {
         private static void Main() {
-            var summary = BenchmarkRunner.Run<GuardBenchmarks>();
+            BenchmarkRunner.Run<GuardBenchmarks>();
         }
     }
 }
