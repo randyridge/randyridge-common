46186a5cda27325b8bf932450d5459f87eff5317
Randy Ridge <randyridge@gmail.com>
2019-12-04 17:06:31 -0500

    added various extension methods

--- RandyRidge.Common.Tests/CommonTestValues.cs
+++ RandyRidge.Common.Tests/CommonTestValues.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RandyRidge.Common {
     public static class CommonTestValues {
         public const string? TestString = "test";
         public const string? WhitespaceString = "   ";
         public static readonly string[]? EmptyArray = Array.Empty<string>();
+        public static readonly IEnumerable<int> EmptyEnumerable = Array.Empty<int>();
         public static readonly string? EmptyString = string.Empty;
         public static readonly string[]? NullArray = null;
+        public static readonly IEnumerable<int> NullEnumerable = null;
         public static readonly int? NullNullableInt = null;
+        public static readonly object? NullObject = null;
         public static readonly string? NullString = null;
         public static readonly string[]? TestArray = {"test"};
+        public static readonly IEnumerable<int> TestEnumerable = Enumerable.Range(1, 3);
         public static readonly int? TestNullableInt = 42;
+        public static readonly object? TestObject = "test";
     }
 }
---
+++ RandyRidge.Common.Tests/ObjectExtensionsTester.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace RandyRidge.Common {
+    public abstract class ObjectExtensionsTester {
+        public sealed class HasValue : ObjectExtensionsTester {
+            [Fact]
+            public void returns_false_on_null() => CommonTestValues.NullObject.HasValue().ShouldBeFalse();
+
+            [Fact]
+            public void returns_true_on_non_null() => CommonTestValues.TestObject.HasValue().ShouldBeTrue();
+        }
+
+        public sealed class IsNull : ObjectExtensionsTester {
+            [Fact]
+            public void returns_false_on_non_null() => CommonTestValues.TestObject.IsNull().ShouldBeFalse();
+
+            [Fact]
+            public void returns_true_on_null() => CommonTestValues.NullObject.IsNull().ShouldBeTrue();
+        }
+    }
+}
