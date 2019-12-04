e099919a0a1ad01c21b53cf2cb32e842e5d6bf5a
Randy Ridge <randyridge@gmail.com>
2019-12-04 10:20:26 -0500

    some guards and extensions for strings

--- RandyRidge.Common/Guard.cs
+++ RandyRidge.Common/Guard.cs
@@ -22,6 +22,9 @@ namespace RandyRidge.Common {
         /// <returns>
         ///     The argument.
         /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="argument" /> is null.
+        /// </exception>
         [DebuggerHidden]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T ArgumentNotNull<T>(T? argument, string argumentName) where T : class => argument ?? throw new ArgumentNullException(argumentName);
@@ -41,8 +44,75 @@ namespace RandyRidge.Common {
         /// <returns>
         ///     The argument.
         /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="argument" /> is null.
+        /// </exception>
         [DebuggerHidden]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T ArgumentNotNull<T>(T? argument, string argumentName) where T : struct => argument ?? throw new ArgumentNullException(argumentName);
+
+        /// <summary>
+        ///     Throws an exception if the tested string argument is null or an empty string.
+        /// </summary>
+        /// <param name="argument">
+        ///     The argument to test.
+        /// </param>
+        /// <param name="argumentName">
+        ///     The name of the argument.
+        /// </param>
+        /// <returns>
+        ///     The argument.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///     Thrown if <paramref name="argument" /> is an empty string.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="argument" /> is null.
+        /// </exception>
+        [DebuggerHidden]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string ArgumentNotNullOrEmpty(string? argument, string argumentName) {
+            if(argument == null) {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            if(argument.Length == 0) {
+                throw new ArgumentException($"'{argumentName}' must not be empty.", argumentName);
+            }
+
+            return argument;
+        }
+
+        /// <summary>
+        ///     Throws an exception if the tested string argument is null, an empty string, or only contains whitespace characters.
+        /// </summary>
+        /// <param name="argument">
+        ///     The argument to test.
+        /// </param>
+        /// <param name="argumentName">
+        ///     The name of the argument.
+        /// </param>
+        /// <returns>
+        ///     The argument.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///     Thrown if <paramref name="argument" /> is an empty string or only contains whitespace characters.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="argument" /> is null.
+        /// </exception>
+        [DebuggerHidden]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string ArgumentNotNullOrWhiteSpace(string? argument, string argumentName) {
+            if(argument == null) {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            if(argument.IsNullOrWhiteSpace()) {
+                throw new ArgumentException($"'{argumentName}' must not be an empty string or contain only whitespace characters.", argumentName);
+            }
+
+            return argument;
+        }
     }
 }
---
+++ RandyRidge.Common/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace RandyRidge.Common {
+    /// <summary>
+    ///     Provides common <see cref="string" /> extensions.
+    /// </summary>
+    public static class StringExtensions {
+        /// <summary>
+        ///     Reports whether the specified string is null or an empty string.
+        /// </summary>
+        /// <param name="text">
+        ///     The <see cref="string" /> to check.
+        /// </param>
+        /// <returns>
+        ///     true if the string is null or an empty string; otherwise, false.
+        /// </returns>
+        [DebuggerHidden]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNullOrEmpty(this string? text) => string.IsNullOrEmpty(text);
+
+        /// <summary>
+        ///     Reports whether the specified string is null, an empty string, or only contains whitespace characters.
+        /// </summary>
+        /// <param name="text">
+        ///     The <see cref="string" /> to check.
+        /// </param>
+        /// <returns>
+        ///     true if the string is null, an empty string, or only contains whitespace characters; otherwise, false.
+        /// </returns>
+        [DebuggerHidden]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNullOrWhiteSpace(this string? text) => string.IsNullOrWhiteSpace(text);
+    }
+}
