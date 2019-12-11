3d92b81f1bd65bbb61752f07c2dc4ec40668da0e
Randy Ridge <randyridge@gmail.com>
2019-12-11 14:01:02 -0500

    add guid extensions, cant get benchmark format the way i want :( (#22)

---
+++ RandyRidge.Common.Benchmarks/GuidExtensionsBenchmarks.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace RandyRidge.Common {
+    [Config(typeof(DefaultBenchmarkConfig))]
+    public class GuidExtensionsBenchmarks {
+        [Benchmark(Baseline = true)]
+        public void ToStringWithDigitsOnly() => Guid.Empty.ToStringWithDigitsOnly();
+    }
+}
---
+++ RandyRidge.Common.Tests/GuidExtensionsTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Shouldly;
+using Xunit;
+
+namespace RandyRidge.Common {
+    public abstract class GuidExtensionsTester {
+        public sealed class ToStringWithDigitsOnly {
+            [Fact]
+            public void formats_correctly() {
+                var guid = Guid.NewGuid();
+                guid.ToStringWithDigitsOnly().ShouldBe(guid.ToString("N", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
