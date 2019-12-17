0a20f0bcba9094e6ce14fa39b5de5bb3ff681047
Randy Ridge <randyridge@gmail.com>
2019-12-16 20:01:36 -0500

    added simple bi-directional dictionary for lookups

---
+++ RandyRidge.Common.Tests/Collections/BidirectionalDictionaryTester.cs
@@ -0,0 +1,42 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace RandyRidge.Common.Collections {
+    public abstract class BidirectionalDictionaryTester {
+        private readonly BidirectionalDictionary<int, string> bidirectional;
+
+        protected BidirectionalDictionaryTester() {
+            bidirectional = new BidirectionalDictionary<int, string>();
+        }
+
+        public class Add : BidirectionalDictionaryTester {
+            [Fact]
+            public void adds_pair() {
+                bidirectional.Add(1, "one");
+                bidirectional.Count.ShouldBe(1);
+            }
+        }
+
+        public sealed class Constructor : BidirectionalDictionaryTester {
+            [Fact]
+            public void sets_left() => bidirectional.Left.ShouldNotBeNull();
+
+            [Fact]
+            public void sets_right() => bidirectional.Right.ShouldNotBeNull();
+        }
+
+        public class Indexer : BidirectionalDictionaryTester {
+            [Fact]
+            public void can_access_left() {
+                bidirectional.Add(1, "one");
+                bidirectional.Left[1].ShouldBe("one");
+            }
+
+            [Fact]
+            public void can_access_right() {
+                bidirectional.Add(1, "one");
+                bidirectional.Right["one"].ShouldBe(1);
+            }
+        }
+    }
+}
