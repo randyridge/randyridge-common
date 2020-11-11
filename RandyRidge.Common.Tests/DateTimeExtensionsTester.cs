a4ae5b7e8fb440ee08c31d898ca77378b6702176
Randy Ridge <randy.ridge@hsn.net>
2020-11-10 19:58:43 -0500

    datetime extensions, update to 5.0 final

---
+++ RandyRidge.Common.Tests/DateTimeExtensionsTester.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace RandyRidge.Common {
+public abstract class DateTimeExtensionsTester {
+        private readonly DateTime testDateTime;
+
+        protected DateTimeExtensionsTester() {
+            testDateTime = new DateTime(1978, 1, 19, 1, 2, 3, 4);
+        }
+
+        public sealed class RoundToDay : DateTimeExtensionsTester {
+            [Fact]
+            public void removes_hours_precision() {
+                var roundedDatetime = testDateTime.RoundToDay();
+                roundedDatetime.Hour.ShouldBe(0);
+            }
+
+            [Fact]
+            public void removes_milliseconds_precision() {
+                var roundedDatetime = testDateTime.RoundToDay();
+                roundedDatetime.Minute.ShouldBe(0);
+            }
+
+            [Fact]
+            public void removes_minutes_precision() {
+                var roundedDatetime = testDateTime.RoundToDay();
+                roundedDatetime.Millisecond.ShouldBe(0);
+            }
+
+            [Fact]
+            public void removes_seconds_precision() {
+                var roundedDatetime = testDateTime.RoundToDay();
+                roundedDatetime.Second.ShouldBe(0);
+            }
+
+            [Fact]
+            public void retains_precision_greater_or_equal_to_minute() {
+                var roundedDatetime = testDateTime.RoundToDay();
+                roundedDatetime.Year.ShouldBe(1978);
+                roundedDatetime.Month.ShouldBe(1);
+                roundedDatetime.Day.ShouldBe(19);
+            }
+        }
+
+        public sealed class RoundToHour : DateTimeExtensionsTester {
+            [Fact]
+            public void removes_milliseconds_precision() {
+                var roundedDatetime = testDateTime.RoundToHour();
+                roundedDatetime.Minute.ShouldBe(0);
+            }
+
+            [Fact]
+            public void removes_minutes_precision() {
+                var roundedDatetime = testDateTime.RoundToHour();
+                roundedDatetime.Millisecond.ShouldBe(0);
+            }
+
+            [Fact]
+            public void removes_seconds_precision() {
+                var roundedDatetime = testDateTime.RoundToHour();
+                roundedDatetime.Second.ShouldBe(0);
+            }
+
+            [Fact]
+            public void retains_precision_greater_or_equal_to_minute() {
+                var roundedDatetime = testDateTime.RoundToHour();
+                roundedDatetime.Year.ShouldBe(1978);
+                roundedDatetime.Month.ShouldBe(1);
+                roundedDatetime.Day.ShouldBe(19);
+                roundedDatetime.Hour.ShouldBe(1);
+            }
+        }
+
+        public sealed class RoundToMinute : DateTimeExtensionsTester {
+            [Fact]
+            public void removes_milliseconds_precision() {
+                var roundedDatetime = testDateTime.RoundToMinute();
+                roundedDatetime.Millisecond.ShouldBe(0);
+            }
+
+            [Fact]
+            public void removes_seconds_precision() {
+                var roundedDatetime = testDateTime.RoundToMinute();
+                roundedDatetime.Second.ShouldBe(0);
+            }
+
+            [Fact]
+            public void retains_precision_greater_or_equal_to_minute() {
+                var roundedDatetime = testDateTime.RoundToMinute();
+                roundedDatetime.Year.ShouldBe(1978);
+                roundedDatetime.Month.ShouldBe(1);
+                roundedDatetime.Day.ShouldBe(19);
+                roundedDatetime.Hour.ShouldBe(1);
+                roundedDatetime.Minute.ShouldBe(2);
+            }
+        }
+
+        public sealed class RoundToSecond : DateTimeExtensionsTester {
+            [Fact]
+            public void removes_milliseconds_precision() {
+                var roundedDatetime = testDateTime.RoundToSecond();
+                roundedDatetime.Millisecond.ShouldBe(0);
+            }
+
+            [Fact]
+            public void retains_precision_greater_or_equal_to_second() {
+                var roundedDatetime = testDateTime.RoundToSecond();
+                roundedDatetime.Year.ShouldBe(1978);
+                roundedDatetime.Month.ShouldBe(1);
+                roundedDatetime.Day.ShouldBe(19);
+                roundedDatetime.Hour.ShouldBe(1);
+                roundedDatetime.Minute.ShouldBe(2);
+                roundedDatetime.Second.ShouldBe(3);
+            }
+        }
+    }}
---
+++ RandyRidge.Common/DateTimeExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandyRidge.Common {
+	/// <summary>
+	///     Provides extension methods for <see cref="DateTime" />.
+	/// </summary>
+	public static class DateTimeExtensions {
+		/// <summary>
+		///     Rounds the specified datetime down to day precision.
+		/// </summary>
+		/// <param name="value">
+		///     The datetime to round down.
+		/// </param>
+		/// <returns>
+		///     The datetime with precision down to the hour.
+		/// </returns>
+		public static DateTime RoundToDay(this DateTime value) => new DateTime(value.Year, value.Month, value.Day, 0, 0, 0);
+
+		/// <summary>
+		///     Rounds the specified datetime down to hour precision.
+		/// </summary>
+		/// <param name="value">
+		///     The datetime to round down.
+		/// </param>
+		/// <returns>
+		///     The datetime with precision down to the hour.
+		/// </returns>
+		public static DateTime RoundToHour(this DateTime value) => new DateTime(value.Year, value.Month, value.Day, value.Hour, 0, 0);
+
+		/// <summary>
+		///     Rounds the specified datetime down to minute precision.
+		/// </summary>
+		/// <param name="value">
+		///     The datetime to round down.
+		/// </param>
+		/// <returns>
+		///     The datetime with precision down to the minute.
+		/// </returns>
+		public static DateTime RoundToMinute(this DateTime value) => new DateTime(value.Year, value.Month, value.Day, value.Hour, value.Minute, 0);
+
+		/// <summary>
+		///     Rounds the specified datetime down to second precision.
+		/// </summary>
+		/// <param name="value">
+		///     The datetime to round down.
+		/// </param>
+		/// <returns>
+		///     The datetime with precision down to the second.
+		/// </returns>
+		public static DateTime RoundToSecond(this DateTime value) => new DateTime(value.Year, value.Month, value.Day, value.Hour, value.Minute, value.Second);
+
+	}
+}
