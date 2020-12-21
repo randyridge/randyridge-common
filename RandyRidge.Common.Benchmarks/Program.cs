62fcf01cdfea67b0683c2bc7a8cb7ac258c34c9b
Randy Ridge <randyridge@gmail.com>
2020-12-21 10:38:33 -0500

    benchmark cleanup

--- RandyRidge.Common.Benchmarks/Program.cs
+++ RandyRidge.Common.Benchmarks/Program.cs
@@ -2,10 +2,7 @@
 
 namespace RandyRidge.Common {
 	internal static class Program {
-		private static void Main() {
-			var benchmarksAssembly = typeof(Program).Assembly;
-			var benchmarkSwitcher = new BenchmarkSwitcher(benchmarksAssembly);
-			benchmarkSwitcher.Run();
-		}
+		private static void Main(string[] args) =>
+			new BenchmarkSwitcher(typeof(Program).Assembly).Run(args);
 	}
 }
---
+++ RandyRidge.Common.Benchmarks/RandomFileBenchmarks.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+using RandyRidge.Common.IO;
+
+namespace RandyRidge.Common {
+	[Config(typeof(DefaultConfig))]
+	public class RandomFileBenchmarks {
+		[Benchmark]
+		public void RandomFile() {
+			using var file = new RandomFile(".jpg");
+			using var file2 = new RandomFile("jpg");
+		}
+	}
+}
